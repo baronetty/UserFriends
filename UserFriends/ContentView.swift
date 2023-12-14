d7ccdcf96fbd6b93334b29f1aa0ddabd30175fe8
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-14 11:45:39 +0200

    Initial Commit

---
+++ UserFriends/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserFriends
+//
+//  Created by Leo  on 14.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserFriends/UserFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserFriendsApp.swift
+//  UserFriends
+//
+//  Created by Leo  on 14.12.23.
+//
+
+import SwiftUI
+
+@main
+struct UserFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
